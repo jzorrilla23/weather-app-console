dace7752b3bb2d2ab03e6b9d815b43dce4b9801f
jzorrilla23 <jzorrilla@intellisys.com.do>
2022-02-15 22:13:29 -0400

    Show the details of the place selected

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { readInput, inquirerMenu, pause } = require('./helpers/inquirer');
+const { readInput, inquirerMenu, pause, showPlaces } = require('./helpers/inquirer');
 const Searchs = require('./models/searchs');
 
 const main = async () => {
@@ -9,8 +9,14 @@ const main = async () => {
         opt = await inquirerMenu();
         switch(opt){
             case 1:
-                const place = await readInput('Please type the place: ');
-                await searchs.city(place);
+                const termn = await readInput('Please type the place: ');
+                const places = await searchs.city(termn);
+                const id = await showPlaces(places);
+                const place = places.find(place => place.id == id);
+                console.log('\nPais Selecionado\n'.green)
+                console.log('City      : ' + place.name)
+                console.log('Latitudt  : ' + place.lat)
+                console.log('Longitudt : ' + place.lng + '\n')
                 break;
             
             case 2:
